c422755be28d07c35af68bc1355b86fd07b71f3e
一滴小雨珠 <kedarui@163.com>
2017-12-29 01:15:34 +0800

    'init'

---
+++ gulpfile.js
@@ -0,0 +1,29 @@
+var gulp = require('gulp');
+
+gulp.task('copy-index',function(){
+	return gulp.src('index.html').pipe(gulp.dest('dist'));
+});
+
+//默认任务
+//gulp.task('default',['copy-index']);
+
+//--'images/*.{png,jpg}'
+//images/*/*
+//images/**/*
+gulp.task('images',function(){
+	return gulp.src('images/**/*').pipe(gulp.dest('dist/images'));	
+});
+
+gulp.task('data',function(){
+	return gulp.src(['xml/*.xml','json/*.json','!json/secret*.json']).pipe(gulp.dest('dist/data'));
+});
+
+gulp.task('build',['copy-index','images','data'],function(){
+	console.log('编译成功！');	
+});
+
+gulp.task('watch',function(){
+	gulp.watch('index.html',['copy-index']);
+	gulp.watch('images/**/*.{jpg,png}',['images']);
+	gulp.watch(['xml/*.xml','json/*.json','!json/secret*.json'],['data']);
+});
